3e48e8dc4152edf327fa7d956be336d3ef106e12
Konsta Purtsi <konsta@identio.fi>
2020-08-08 16:10:45 +0300

    Fix todo and horizontal scroll.

--- src/components/todo.js
+++ src/components/todo.js
@@ -4,14 +4,14 @@ const Todo = () => (
   <div className="Todo">
     <ul>
       <li>Ilmoittaudu yliopistoon ja ylioppilaskuntaan, ohjeet löydät <Link href="https://www.utu.fi/fi/opiskelijaksi/ilmo">täältä.</Link></li>
-      <li>Tilaa opiskelijakortti <Link href="https://www.frank.fi/opiskelijakortti/">Frankista</Link>, tärkeimpänä syynä 3,06€ hintainen opiskelijaruoka.</li>
+      <li>Tilaa opiskelijakortti <Link href="https://www.frank.fi/opiskelijakortti/">Frankista</Link>, tärkeimpänä syynä 2,80€ hintainen opiskelijaruoka.</li>
       <li>
         Hanki asumispaikka <strong>jo heti</strong> ensimmäiselle viikolle.
         Kannattaa hakea asuntoa <Link href="https://tys.fi/">TYS:ltä</Link>, uudet opiskelijat ohittelevat jonossa.
         Jos TYS:ltä ei kuulu mitään pariin kuukauteen, kannattaa sinne soittaa.
       </li>
       <li>Olet syyskuusta eteenpäin oikeutettu saamaan opintotukea ja yleistä asumistukea. Lisää tietoa <Link href="https://www.kela.fi/opintotuki">täällä</Link>.</li>
-      <li>Osallistu varaslähtöön lauantaina 22.8. kello 12:30-> paikassa Q-talo, Nummenpuistokatu 2.</li>
+      <li>Osallistu varaslähtöön lauantaina 22.8. kello 14:30-> paikassa Q-talo, Nummenpuistokatu 2.</li>
       <li>Tutustu orientaatioviikon aikatauluun. Aikataulu ilmestyy näille sivuille.</li>
     </ul>
   </div>
